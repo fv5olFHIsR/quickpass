9b568541c764c5c3428daf2228df14d50b2d6653
KK DD <kk@kk.com>
2018-05-13 20:58:41 +0300

    Test this commit

--- quickpass.php
+++ quickpass.php
@@ -6,7 +6,7 @@ class QuickPass{
 			for ($i = 0; $i < $height; $i++) echo PHP_EOL;
 		}
 		clearStdin();
-		function generateRandomString($length = 22) {
+		function generateRandomString($length = 20) {
 			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 			$charactersLength = strlen($characters);
 			$randomString = '';
